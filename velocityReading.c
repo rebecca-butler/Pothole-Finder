f60435ea7984cad69790b49bc331834c074b27d1
Rebecca Butler <rebutler@edu.uwaterloo.ca>
2019-05-27 18:52:04 -0400

    Formatted velocityReadings.c

--- velocityReading.c
+++ velocityReading.c
@@ -1,7 +1,7 @@
 /*****************************************************************************/
-//	Function:    Cpp file for ITG3200 
-//  Hardware:    Grove - 3-Axis Digital Gyro
-//	Arduino IDE: Arduino-1.0
+// Function:    Cpp file for ITG3200 
+// Hardware:    Grove - 3-Axis Digital Gyro
+// Arduino IDE: Arduino-1.0
 /*******************************************************************************/
 
 #include <Arduino.h>
@@ -9,9 +9,9 @@
 #include "ITG3200.h"
 
 /**********************************************************************/
-/*Function: Read a byte with the register address of ITG3200.         */
-/*Parameter:-uint8_t _register,the register address  of ITG3200 to read; */
-/*Return:	-int8_t,the byte that is read from the register.		  */
+/* Function: 	Read a byte with the register address of ITG3200. */
+/* Parameter:	-uint8_t _register,the register address  of ITG3200 to read; */
+/* Return:	-int8_t,the byte that is read from the register. */
 int8_t ITG3200::read(uint8_t _register)
 {
     int8_t data;
@@ -26,7 +26,8 @@ int8_t ITG3200::read(uint8_t _register)
 	Wire.endTransmission();
     return data;
 }
-/*Function: Write a byte to the register of the MMA7660*/
+
+/* Function: Write a byte to the register of the MMA7660 */
 void ITG3200::write(uint8_t _register, uint8_t _data)
 {
 	Wire.begin();
@@ -37,7 +38,7 @@ void ITG3200::write(uint8_t _register, uint8_t _data)
 }
 
 /**********************************************************************/
-/*Function: Initialization for ITG3200.         					  */
+/* Function: Initialization for ITG3200. */
 void ITG3200::init()
 {
 	Wire.begin();
@@ -69,25 +70,16 @@ int16_t ITG3200::read(uint8_t addressh, uint8_t addressl)
     }
     return data;
 }
-/*Function: Get the temperature from ITG3200 that with a on-chip*/
-/*           temperature sensor.                                */
-double ITG3200::getTemperature()
-{
-	int temp;
-	double temperature;
-	temp = read(ITG3200_TMP_H, ITG3200_TMP_L);
-	temperature = 35+ ((double) (temp + 13200)) / 280;
-	return(temperature);
-}
-/*Function: Get the contents of the registers in the ITG3200*/
-/*          so as to calculate the angular velocity.        */
+
+/* Function: Get the contents of the registers in the ITG3200 so as to calculate the angular velocity. */
 void ITG3200::getXYZ(int16_t *x,int16_t *y,int16_t *z)
 {
 	*x = read(ITG3200_GX_H,ITG3200_GX_L)+x_offset;
 	*y = read(ITG3200_GY_H,ITG3200_GY_L)+y_offset;
 	*z = read(ITG3200_GZ_H,ITG3200_GZ_L)+z_offset;
 }
-/*Function: Get the angular velocity and its unit is degree per second.*/
+
+/* Function: Get the angular velocity and its unit is degree per second. */
 void ITG3200::getAngularVelocity(float *ax,float *ay,float *az)
 {
 	int16_t x,y,z;
@@ -96,6 +88,7 @@ void ITG3200::getAngularVelocity(float *ax,float *ay,float *az)
 	*ay = y/14.375;
 	*az = z/14.375;
 }
+
 void ITG3200::zeroCalibrate(unsigned int samples, unsigned int sampleDelayMS) 
 {
   int16_t x_offset_temp = 0;
